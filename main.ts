9fddfb48b6d74bd2249cef31b24aeeddc8ab20c0
scary-gerbil <71849976+scary-gerbil@users.noreply.github.com>
2020-09-24 22:49:45 +0200

    v2

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 music.playTone(262, music.beat(BeatFraction.Whole))
 music.playTone(440, music.beat(BeatFraction.Half))
 music.playTone(440, music.beat(BeatFraction.Half))
+music.playTone(262, music.beat(BeatFraction.Whole))
+music.playTone(262, music.beat(BeatFraction.Whole))
+music.playTone(262, music.beat(BeatFraction.Whole))
 basic.forever(function () {
 	
 })
