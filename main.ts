897b868816bf1c913de8ea641596abbdef9780f4
scary-gerbil <71849976+scary-gerbil@users.noreply.github.com>
2020-09-24 23:01:03 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
-music.playTone(262, music.beat(BeatFraction.Whole))
-music.playTone(440, music.beat(BeatFraction.Half))
-music.playTone(440, music.beat(BeatFraction.Half))
-music.playTone(262, music.beat(BeatFraction.Whole))
-music.playTone(262, music.beat(BeatFraction.Whole))
-music.playTone(262, music.beat(BeatFraction.Whole))
+for (let index = 0; index < 4; index++) {
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+}
 basic.forever(function () {
-	
+    basic.showIcon(IconNames.Heart)
 })
