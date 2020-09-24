b8d08098e91f786100a973cdd99ff8312901f54a
scary-gerbil <71849976+scary-gerbil@users.noreply.github.com>
2020-09-24 22:32:26 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,6 @@
+music.playTone(262, music.beat(BeatFraction.Whole))
+music.playTone(440, music.beat(BeatFraction.Half))
+music.playTone(440, music.beat(BeatFraction.Half))
+basic.forever(function () {
+	
+})
